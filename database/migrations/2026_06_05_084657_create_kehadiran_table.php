a3ad26ee64c408e220ab10509430cf674dfc46af
Mellow <savinalarissa@student.ub.ac.id>
2026-06-07 00:04:24 +0700

    Migration

--- database/migrations/2026_06_05_084657_create_kehadiran_table.php
+++ database/migrations/2026_06_05_084657_create_kehadiran_table.php
@@ -11,38 +11,38 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('kehadiran', function (Blueprint $table) {
-            $table->id('id_kehadiran');
+        // Schema::create('kehadiran', function (Blueprint $table) {
+        //     $table->id('id_kehadiran');
 
-            $table->unsignedBigInteger('id_mahasiswa');
-            $table->unsignedBigInteger('id_kelas');
+        //     $table->unsignedBigInteger('id_mahasiswa');
+        //     $table->unsignedBigInteger('id_kelas');
 
-            $table->dateTime('waktu_absen');
+        //     $table->dateTime('waktu_absen');
 
-            $table->decimal('latitude', 10, 8)->nullable();
-            $table->decimal('longitude', 11, 8)->nullable();
+        //     $table->decimal('latitude', 10, 8)->nullable();
+        //     $table->decimal('longitude', 11, 8)->nullable();
 
-            $table->string('foto_url', 500)->nullable();
+        //     $table->string('foto_url', 500)->nullable();
 
-            $table->enum('status', [
-                'Hadir',
-                'Izin',
-                'Sakit',
-                'Alpha'
-            ])->default('Hadir');
+        //     $table->enum('status', [
+        //         'Hadir',
+        //         'Izin',
+        //         'Sakit',
+        //         'Alpha'
+        //     ])->default('Hadir');
 
-            $table->foreign('id_mahasiswa')
-                ->references('id_mahasiswa')
-                ->on('mahasiswa')
-                ->onDelete('cascade');
+        //     $table->foreign('id_mahasiswa')
+        //         ->references('id_mahasiswa')
+        //         ->on('mahasiswa')
+        //         ->onDelete('cascade');
 
-            $table->foreign('id_kelas')
-                ->references('id_kelas')
-                ->on('kelas')
-                ->onDelete('cascade');
+        //     $table->foreign('id_kelas')
+        //         ->references('id_kelas')
+        //         ->on('kelas')
+        //         ->onDelete('cascade');
 
-            $table->timestamps();
-        });
+        //     $table->timestamps();
+        // });
     }
 
     /**
